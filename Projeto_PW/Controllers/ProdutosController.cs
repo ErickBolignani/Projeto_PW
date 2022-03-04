66346f14c7b9aa49f3be3adb96918f5c4c4fc464
ErickBolignani <bolignaniap255591@gmail.com>
2022-03-04 20:26:46 -0300

    Criação do ComboBox

--- Projeto_PW/Controllers/ProdutosController.cs
+++ Projeto_PW/Controllers/ProdutosController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Projeto_PW.Models;
+using System;
 using System.Collections.Generic;
 
 namespace Projeto_PW.Controllers
@@ -7,20 +8,44 @@ namespace Projeto_PW.Controllers
     public class ProdutosController : Controller
     {
         public static List<ProdutoModel> lsProdutos = new List<ProdutoModel>();
+
+        public static List<CategoriaModel> lsCategorias = new List<CategoriaModel>();
         public IActionResult Index()
         {
-            ProdutoModel produto = new ProdutoModel();
-            produto.Descricao = "Mouse Gamer";
-            produto.Codigo = 10;
-
-            lsProdutos.Add(produto);
-
-            return View(lsProdutos);
+          
+            return View();
         }
 
         public IActionResult Create()
         {
-            return View();
+            
+            ProdutoModel produto = new ProdutoModel();
+            produto.Descricao = "Mouse Gamer";
+            produto.Codigo = 10;
+            produto.DataVencimento = DateTime.Now;
+            ListagemCategorias();
+
+            produto.ListaCategoriaDisponiveis = lsCategorias;
+            
+            
+            
+            return View(produto);
+        }
+
+        private List<CategoriaModel> ListagemCategorias()
+        {
+            CategoriaModel categoria1 = new CategoriaModel();
+            categoria1.CategoriaId = 10;
+            categoria1.Nome = "Padaria";
+
+            CategoriaModel categoria2 = new CategoriaModel();
+            categoria2.CategoriaId = 20;
+            categoria2.Nome = "Açougue";
+
+            lsCategorias.Add(categoria1);
+            lsCategorias.Add(categoria2);
+
+            return lsCategorias;
         }
 
         [HttpPost]
